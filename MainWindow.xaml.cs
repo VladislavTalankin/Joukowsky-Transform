065be5aa1f93667a833483c5917d30b179060529
VladislavTalankin <32197942+VladislavTalankin@users.noreply.github.com>
2021-06-05 15:15:02 +0500

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace Joukowsky_transform
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            Update();
         }
 
         void drawline(Complex start, Complex end)
@@ -48,8 +48,8 @@ namespace Joukowsky_transform
         {
             MainPanel.Children.Clear();
             Line vert = new Line();
-            vert.X1 = Math.Floor(MainPanel.ActualWidth / 2);
-            vert.X2 = Math.Floor(MainPanel.ActualWidth / 2);
+            vert.X1 = 130;
+            vert.X2 = 130;
             vert.Y1 = 0;
             vert.Y2 = Math.Floor(MainPanel.ActualHeight);
             vert.Stroke = Brushes.Black;
@@ -60,6 +60,13 @@ namespace Joukowsky_transform
             hor.Y1 = -Math.Floor(MainPanel.ActualHeight / 2);
             hor.Y2 = -Math.Floor(MainPanel.ActualHeight / 2);
             hor.Stroke = Brushes.Black;
+            Line scls = new Line();
+            scls.X1 = 1130;
+            scls.X2 = 1130;
+            scls.Y1 = -Math.Floor(MainPanel.ActualHeight / 2) + 10;
+            scls.Y2 = -Math.Floor(MainPanel.ActualHeight / 2) - 10;
+            scls.Stroke = Brushes.Black;
+            MainPanel.Children.Add(scls);
             MainPanel.Children.Add(hor);
         }
 
@@ -80,7 +87,8 @@ namespace Joukowsky_transform
             
             return S3(S2(S1(z)));
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        void Update()
         {
             Cln();
             double phi = 0;
@@ -102,6 +110,51 @@ namespace Joukowsky_transform
                 drawline(source[i], source[i + 1]);
             }
             drawline(source[source.Length - 1], source[0]);
+        }
+
+        private void centerX_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double text = Convert.ToDouble(centerX.Text);
+            if (e.Delta > 0)
+            {
+                text += 0.01;
+            }
+            else
+            {
+                text -= 0.01;
+            }
+            centerX.Text = text.ToString();
+            Update();
+        }
+
+        private void centerY_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double text = Convert.ToDouble(centerY.Text);
+            if (e.Delta > 0)
+            {
+                text += 0.01;
+            }
+            else
+            {
+                text -= 0.01;
+            }
+            centerY.Text = text.ToString();
+            Update();
+        }
+
+        private void circleR_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double text = Convert.ToDouble(circleR.Text);
+            if (e.Delta > 0)
+            {
+                text += 0.01;
+            }
+            else
+            {
+                text -= 0.01;
+            }
+            circleR.Text = text.ToString();
+            Update();
         }
     }
 }
